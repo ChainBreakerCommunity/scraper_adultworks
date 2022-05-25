b60739ad8d101366d149bc78bac8f910da9005c3
juanchobanano <juancepeda.gestion@gmail.com>
2022-05-25 11:08:00 +0300

    Cambios

---
+++ app/constants.py
@@ -0,0 +1,8 @@
+# Sitio, urls, categorías y variables.
+AUTHOR = "chainbreaker"
+COUNTRY = "uk"
+SITE_NAME = "adultworks"
+SITE = "https://adultwork.com/"
+LANGUAGE = "english"
+MAX_PAGES = 100 # El máximo es 40.
+CATEGORY = "escorts"
